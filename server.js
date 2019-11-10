f86ad0a9b0c16e39db723bac83c4d89b0eb789e4
Magnus Hedvall <magnus@hdvll.com>
2019-11-10 18:18:33 +0100

    App built, ready for deploy

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
-const port = process.env.port || 5000;
 
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello world!'));
 app.use('/api/search', require('./api/search'));
 app.use('/api/profile', require('./api/getProfileAndCharacters'));
 app.use('/api/stats', require('./api/getProfileOrCharacterStats'));
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+// Production assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+// Port
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
